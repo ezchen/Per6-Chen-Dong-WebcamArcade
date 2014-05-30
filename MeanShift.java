6013eb447db4d0d1f6cc94bd83bf0ebd93c6642e
ezchen <echen2015@gmail.com>
2014-05-29 22:22:24 -0400

    class that will track the colored object

---
+++ MeanShift.java
@@ -0,0 +1,13 @@
+public class MeanShift {
+
+	int[] colors;
+	public MeanShift(int[] rgbColors) {
+		colors = rgbColors;
+	}
+
+	public int[] meanShift(int x, int y, int radius) {
+	}
+
+	public static void main(String[] args) {
+	}
+}
